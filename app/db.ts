b8ec00a1e7a00d7136cd9da1b7c811de18d4eb45
Wonu Lee <wonwoolee24@gmail.com>
2025-10-05 03:50:28 +0900

    add query duration logging

--- app/db.ts
+++ app/db.ts
@@ -1,5 +1,35 @@
+import chalk from 'chalk'
+import { remember } from '@epic-web/remember'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { PrismaClient } from '~/generated/prisma/client'
 
-export const prisma = new PrismaClient()
-  .$extends(withAccelerate())
+export const prisma = remember('prisma', () => {
+	// NOTE: if you change anything in this function you'll need to restart
+	// the dev server to see your changes.
+	const logThreshold = 20
+
+	const client = new PrismaClient({
+		log: [
+			{ level: 'query', emit: 'event' },
+			{ level: 'error', emit: 'stdout' },
+			{ level: 'warn', emit: 'stdout' },
+		],
+	})
+	client.$on('query', async (e) => {
+		if (e.duration < logThreshold) return
+		const color =
+			e.duration < logThreshold * 1.1
+				? 'green'
+				: e.duration < logThreshold * 1.2
+					? 'blue'
+					: e.duration < logThreshold * 1.3
+						? 'yellow'
+						: e.duration < logThreshold * 1.4
+							? 'redBright'
+							: 'red'
+		const dur = chalk[color](`${e.duration}ms`)
+		console.info(`prisma:query - ${dur} - ${e.query}`)
+	})
+	client.$extends(withAccelerate()).$connect()
+	return client
+})
